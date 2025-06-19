b5f8c75be0819aede6fb9b0a4f6a689b78bdd573
jamaliyaati <jamaiyaati@gmail.com>
2025-06-19 08:47:04 +0500

    update

---
+++ dowhile.cpp
@@ -0,0 +1,19 @@
+//
+// Created by ebrahim on 6/18/25.
+//
+#include <iostream>
+
+using namespace std;
+int main() {
+
+    double num;
+
+  do {
+      cout<<"enter a number: ";
+      cin>>num;
+
+  }while(num<0);
+
+
+    return 0;
+}

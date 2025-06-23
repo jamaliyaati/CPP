80dec38d02e5874b28117e29862fc3661b5b4ae0
jamaliyaati <jamaiyaati@gmail.com>
2025-06-23 22:48:43 +0500

    update

---
+++ game.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+int main(){
+cout << "-----------------game--------------------"<<endl;
+    srand(time(NULL));
+    char sel;
+    char cpu_s;
+    int num = (rand()%3 +1);
+
+    
+    cout<<"ENTER      R    for rock"<<endl;
+    cout<<"ENTER      P    for paper"<<endl;
+    cout<<"ENTER      S    for scissor"<<endl;
+    cout<<": ";
+    cin >> sel;
+    sel = tolower(sel);
+    
+    switch(num){
+        case 1 :
+            cpu_s = 'r';
+            if(sel == cpu_s){
+                cout<<"DRAWN, both chose Rock";
+            }else if (sel == 'p'){
+                cout<<"YOU WIN, you chose Paper and CPU chose Rock";
+            }else{
+                cout<<"YOU LOSE, you chose Scissor and CPU chose Rock";
+            }
+            break;
+        case 2 :
+            cpu_s = 'p';
+            if(sel == cpu_s){
+                cout<<"DRAWN, both chose Paper";
+            }else if (sel == 's'){
+                cout<<"YOU WIN, you chose Scissor and CPU chose Paper";
+            }else{
+                cout<<"YOU LOSE, you chose Rock and CPU chose Paper";
+            }
+            break;
+        case 3 :
+            cpu_s = 's';
+            if(sel == cpu_s){
+                cout<<"DRAWN, both chose Scissor";
+            }else if (sel == 'r'){
+                cout<<"YOU WIN, you chose Rock and CPU chose Scissor";
+            }else{
+                cout<<"YOU LOSE, you chose Paper and CPU chose Scissor";
+            }
+            break;
+    }
+cout << "------------------end----------------------";
+return 0;
+}
